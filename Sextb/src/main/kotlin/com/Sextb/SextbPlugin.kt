99fc8292a51b018c14d94cdbdd673fee55188a74
mcvo1705 <51687927+mcvo1705@users.noreply.github.com>
2026-05-16 20:43:40 +0700

    Refactor SextbPlugin class and clean up code
    Refactor SextbPlugin class to remove unused imports and commented code.

--- Sextb/src/main/kotlin/com/Sextb/SextbPlugin.kt
+++ Sextb/src/main/kotlin/com/Sextb/SextbPlugin.kt
@@ -2,17 +2,18 @@ package com.Sextb
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
-import com.Sextb.Stbturbo
 import com.lagradost.cloudstream3.extractors.StreamTape
-import com.lagradost.cloudstream3.extractors.Wishonly
 
 @CloudstreamPlugin
-class SextbPlugin: Plugin() {
-    override fun load(context: Context) {
+class SextbPlugin : Plugin() {
+
+    override fun load() {
+
         registerMainAPI(SextbProvider())
+
         registerExtractorAPI(StreamTape())
-        //registerExtractorAPI(Wishonly())
-        registerExtractorAPI(Stbturbo())
+
+        // registerExtractorAPI(Stbturbo())
+        // registerExtractorAPI(Wishonly())
     }
 }
